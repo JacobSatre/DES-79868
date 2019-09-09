9047bd3fd6008305cc2cb4cabad8793841020d57
JacobSatre <53311394+JacobSatre@users.noreply.github.com>
2019-09-09 15:19:56 -0400

    Added IE support (remove function polyfill)

--- javascript.js
+++ javascript.js
@@ -1,3 +1,24 @@
+//IE remove function polyfill
+(function (arr) {
+  arr.forEach(function (item) {
+    if (item.hasOwnProperty('remove')) {
+      return;
+    }
+    Object.defineProperty(item, 'remove', {
+      configurable: true,
+      enumerable: true,
+      writable: true,
+      value: function remove() {
+        if (this.parentNode === null) {
+          return;
+        }
+        this.parentNode.removeChild(this);
+      }
+    });
+  });
+})([Element.prototype, CharacterData.prototype, DocumentType.prototype]);
+
+
 $(document).ready(function() {
 
         //retrieve submit button list
